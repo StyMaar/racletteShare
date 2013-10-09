670d9adc080c5f6210fb7b15387420bb38439b7e
StyMaar <inscription@stymaar.fr>
2013-10-09 15:56:37 +0200

    page de connexion terminée +correction d'un bug qui faisait crasher le serveur si jamais la base de données ne répondait pas.

--- angular/controllers.js
+++ angular/controllers.js
@@ -3,6 +3,16 @@
 /* Controllers */
 
 angular.module('controllers', ['racletteModules']).
-  controller('connexionCtrl', ['$scope', function($scope) {
-
+  controller('connexionCtrl', ['$scope','$http','$location', function($scope,$http,$location) {
+	$scope.hiddenMessage = true;
+	$scope.errorMessage = "";
+	$scope.connexionClick = function(){
+		$http.get('users/'+$scope.login+'/'+$scope.password).success(function() {
+			$location.path("/");
+			$location.replace();
+		}).error(function(data, status, headers) {
+			$scope.hiddenMessage = false;
+			$scope.errorMessage = "Connexion impossible, veuillez réessayer."
+		});
+	}
   }]);
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var mysql = require('mysql');
-var pool  = mysql.createPool({
-        host:'localhost',
-        user:'root',
+var pool = mysql.createPool({
+	host:'localhost',
+	user:'root',
         password:'azerty',
-        database:'raclette'
+	database:'raclette'
 });
 var uuid = require('node-uuid').v4;
 var async = require('async');
+var check = require('validator').check;
 
 var app = express();
 
@@ -16,9 +17,8 @@ var app = express();
 // scr : http://blog.modulus.io/nodejs-and-express-sessions + l doc de connect sur les sessions pour la durée d'ouverture de la session
 app.use(express.cookieParser());
 app.use(express.session({secret:"1234poney", cookie:{path: '/', httpOnly: true, maxAge: 8640000000 }})); //la session reste ouverte pendant 100 jours par défaut
+// Rq : pour accédr aux infos de la session : req.session.bob 
 //end sessions
-
-//pour accédr aux infos de la session : req.session.bob 
 
 //pour lire le contenu d'une requête post ou PUT
 app.use(express.bodyParser());
@@ -27,54 +27,132 @@ app.use(express.bodyParser());
 //pour les demandes de fichier statiques, sauf le index.html
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/angular", express.static(__dirname + '/angular'));
-app.use("/libs_js", express.static(__dirname + '/lib'));
+app.use("/libs_js", express.static(__dirname + '/libs_js'));
 app.use("/img", express.static(__dirname + '/img'));
 
 ///////////////////////////////////////
 // fonctions utilitaires (elle partiront dans un module à part plus tard)
 var utils = {};
-utils.forbiden = function(req,res){
-  res.send(401,"vous n'avez pas la permission d'accéder à cette interface");
+
+utils.notFound = function(res){
+	res.send(404); 
 }
 
-utils.notFound = function(req,res){
-  res.send(404) //,"page non trouvée"); //le "page non trouvée" n'est là que parce que express semble ne pas renvoyer de réponse si jamais on ne met pas de corps de réponse.
+utils.badRequest = function(res){
+	res.send(400); 
 }
 
-utils.error = function(req,res,err){
-  if(!err.notFound){
-    res.send(500,err);
-  }
+utils.forbiden = function(res){
+	res.send(401,"vous n'avez pas la permission d'accéder à cette interface");
+}
+
+utils.error = function(res,err){
+	res.send(500,err); 
+}
+// fonction qui sert à évaluer les erreurs
+// return true s'il n'y a pas d'erreur
+// mais s'il y a une erreur, elle envoie une réponse http avec le bon code d'erreur et retourne false
+utils.checkError = function(res,err){
+	switch(err){
+		case null :
+		case "" :
+		break;
+		case "notFound" : 
+			utils.notFound (res);
+		break;
+		case "badRequest":
+			utils.badRequest(res);
+		break;
+		case "forbiden":
+			utils.forbiden(res);
+		break;
+		default:
+			utils.error(res,err);
+		break;
+	}
+	return !err;
 }
 
 utils.ok = function(req,res){
-  res.send(200)//,"ok"); //le Ok n'est là que parce que express semble ne pas renvoyer de réponse si jamais on ne met pas de corps de réponse.
+	res.send(200);
+}
+utils.created = function(req,res){
+	res.send(201);
 }
 
 //retourne un uuid sous forme de string
 utils.uuid = function(){
-  var buff = new Buffer(32);
-  uuid(null,buff);
-  return uuid.unparse(buff);
+	var buff = new Buffer(32);
+	uuid(null,buff);
+	return uuid.unparse(buff);
 }
 
-/* vérifie si une erreur est retournée par le SGBD,
-puis, s'il n'y en a pas : vérifie si tout c'est bien passé
-  si ce n'est pas le cas on retourne une erreur : "pas d'écriture dans la base de données"
+/* Pour contrôler les écritures en base : 
+on vérifie si une erreur est retournée par le SGBD,
+puis, s'il n'y en a pas : vérifie que quelque chose a été écrit 
+si ce n'est pas le cas on retourne une erreur : "pas d'écriture dans la base de données"
 */
 utils.checkUpdateErr = function (err,results){
-  if(!err && (!results || (results && results.affectedRows == 0))){
-    err = "pas d'écriture dans la base de données";
-  }
-  return err;
+	if(!err && (!results || (results && results.affectedRows == 0))){
+		err = "pas d'écriture dans la base de données";
+	}
+	return err;
 }
+
 
 ////////////////////////////////////////
 
 //traitement statique du index.html
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/index.html');
 });
+
+
+/*
+	Connexion
+*/
+app.get("/users/:login/:password",function(req,res){
+	var login = req.params.login;
+	var password = req.params.password;
+	try {
+		check(login).len(3,64);
+		check(password).len(3,64);
+	} catch (e){
+		utils.badRequest(res);
+		return;
+	}
+	//ici l'utilisation de async n'est pas indispensable, mais par soucis de cohérence de l'ensemble je l'utilise quand même
+	async.parallel([doLogin(login,password)],function(err,id){
+		if(utils.checkError(res,err)){
+			req.session.user_uuid=id;
+			utils.ok(req,res);				
+		}
+	});
+});
+
+function doLogin(login, password){
+	return function(callback){ 
+		pool.getConnection(function(err,connection){
+			//on s'assure que l'appel d'un connection dans le pool se passe bien.
+			if(err){
+				callback(err);
+				return;
+			}
+			connection.query('SELECT uuid as id FROM user WHERE name = ? AND password = SHA2(?, 224)', [login,password], function(err, rows) {
+				connection.release();//on libère la connexion pour la remettre dans le pool dès qu'on n'en a plus besoin
+				var id = null;
+				if(!err){
+					if(rows && rows.length != 0){
+							id = rows[0].id;
+					}else{
+						err = "forbiden";
+					}
+				}
+				callback(err,id);
+			});
+		});
+	}
+}
 
 
 app.listen(process.argv[2]||7777); //si jamais un numéro de port est passé en paramètre à l'execution du script node, alors on utilisera ce port là, sinon on utilise le port 7777 par défaut
