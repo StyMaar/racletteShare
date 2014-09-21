b8f93a352d33cafc4011fa82888cc47319650e11
StyMaar <inscription@stymaar.fr>
2014-09-21 23:36:11 +0200

    Mise à jour du modèle de données
    Mise en place de clés étrangère (pour éviter les incohérences)

--- raclette.sql
+++ raclette.sql
@@ -27,7 +27,7 @@ SET time_zone = "+01:00";
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `label` char(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
@@ -46,25 +46,24 @@ INSERT INTO `category` (`id`, `label`) VALUES
 (6, 'Maison/Cuisine'),
 (7, 'Culture');
 
--- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(36) NOT NULL,
-  `date` date NOT NULL,
-  `sender_uuid` varchar(36) NOT NULL,
-  `target_uuid` varchar(36) NOT NULL,
-  `grade` int(11) NOT NULL,
-  `comment` varchar(140) NOT NULL,
-  `item_uuid` varchar(36) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(36) NOT NULL,
+  `login` varchar(64) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `password` char(56) NOT NULL,
+  `city` varchar(64) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `target` (`target_uuid`),
-  KEY `sender` (`sender_uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `nickname` (`name`),
+  UNIQUE KEY `login` (`login`),
+  KEY `ville` (`city`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 --
 -- Table structure for table `item`
@@ -77,7 +76,10 @@ CREATE TABLE IF NOT EXISTS `item` (
   `description` varchar(255) DEFAULT NULL,
   `category` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_ID` (`user_id`)
+  FOREIGN KEY `user_ID` (`user_id`) REFERENCES user(id)
+                     ON DELETE CASCADE,
+  FOREIGN KEY `category_ID` (`category`) REFERENCES category(id)
+                     ON DELETE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 -- avec InnoDB on a des transactions, mais pas d'index full texte dans cette version de mysql (à partir de la 5.6 seulement)
 -- TODO : mettre à jour mySQL et rajouter cet index FULLTEXT
@@ -97,13 +99,40 @@ CREATE TABLE IF NOT EXISTS `demande` (
   `category` int(10) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_ID` (`user_id`)
+  FOREIGN KEY `user_ID` (`user_id`) REFERENCES user(id)
+                     ON DELETE CASCADE,
+  FOREIGN KEY `category` (`category`) REFERENCES category(id)
+                     ON DELETE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 -- avec InnoDB on a des transactions, mais pas d'index full texte dans cette version de mysql (à partir de la 5.6 seulement)
 -- TODO : mettre à jour mySQL et rajouter cet index FULLTEXT
 -- ,
 -- FULLTEXT KEY `rechercheNom` (`name`,`description`)
 -- ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) NOT NULL,
+  `date` date NOT NULL,
+  `sender_uuid` varchar(36) NOT NULL,
+  `target_uuid` varchar(36) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `comment` varchar(140) NOT NULL,
+  `item_uuid` varchar(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY `target` (`target_uuid`) REFERENCES user(id)
+                     ON DELETE CASCADE,
+  FOREIGN KEY `sender` (`sender_uuid`) REFERENCES user(id)
+                     ON DELETE CASCADE,
+  FOREIGN KEY `object_ID` (`item_uuid`) REFERENCES item(id)
+                     ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Table structure for table `message`
@@ -117,26 +146,13 @@ CREATE TABLE IF NOT EXISTS `message` (
   `date` datetime NOT NULL,
   `receiver_id` varchar(36) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_ID` (`sender_id`),
-  KEY `object_ID` (`item_id`)
+  FOREIGN KEY `sender_ID` (`sender_id`) REFERENCES user(id)
+                     ON DELETE CASCADE,
+  FOREIGN KEY `receiver_ID` (`receiver_id`) REFERENCES user(id)
+                    ON DELETE CASCADE,
+  FOREIGN KEY `object_ID` (`item_id`) REFERENCES item(id)
+                     ON DELETE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=58 ;
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` varchar(36) NOT NULL,
-  `login` varchar(64) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `password` char(56) NOT NULL,
-  `city` varchar(64) DEFAULT NULL,
-  `tel` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nickname` (`name`),
-  UNIQUE KEY `login` (`login`),
-  KEY `ville` (`city`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
