369b90d729af06e2b72f7bb71a7ad48cfddf70f9
StyMaar <inscription@stymaar.fr>
2015-04-18 04:05:45 -0400

    added a sql script to purge the database after a non transactionnal test

---
+++ cleanbase.sql
@@ -0,0 +1,12 @@
+-- must be used after a non transactionnal test
+-- n.b. you need to disable the transactions if you want to make tests on full texts indexes
+
+DELETE FROM user;
+DELETE FROM item;
+DELETE FROM demande;
+DELETE FROM message;
+
+
+
+
+
